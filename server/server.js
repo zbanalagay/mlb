dfe31954b9f1abe2087f7d5bbc3e1fb6e5727055
Elizabeth Banalagay <zbanalagay@gmail.com>
2016-05-03 18:55:36 -0700

    made server to serve static files in server/server.js

--- server/server.js
+++ server/server.js
@@ -1,1 +1,7 @@
-//Initial commit
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 3000;
+app.use(express.static(__dirname + './../client'));
+
+app.listen(port);
+console.log('Now listening on port: ' + port);
