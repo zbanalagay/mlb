0097de6582dc392b251945a3d2a69dd892d0ac3d
Elizabeth Banalagay <zbanalagay@gmail.com>
2016-05-05 17:07:33 -0700

    removed canvas from app.js

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,19 +1,17 @@
 ;(function(){
   'use strict';
 
-  mlb.canvas.width = window.innerWidth;
   mlb.getJSON("http://gdx.mlb.com/components/game/mlb/year_2016/month_04/day_29/master_scoreboard.json", responseIsReturned);
-
 
   function responseIsReturned(err, response){
     if(err !== null){
       console.error(err);
     } else{
-      populateCanvas(response);
+      populateContainer(response);
     }
   }
 
-  function populateCanvas(response){
+  function populateContainer(response){
     var gamesArray = [];
     for(var i = 0; i<response.data.games.game.length; i++){
       var gameObj = {};
@@ -27,49 +25,45 @@
     }
     console.log(gamesArray);
 
-    if(mlb.canvas.getContext){
-      var context = mlb.canvas.getContext('2d');
-
       for(var j = 0; j<gamesArray.length; j++){
-        var img = new Image();
-        img.src = gamesArray[j].videoThumbnails[0].content;
-        img.width = gamesArray[j].videoThumbnails[0].width;
-        img.height = gamesArray[j].videoThumbnails[0].height;
-        img.setAttribute('crossOrigin', '');
-        img.onload = (function(img,j){
-            return function(){
-              context.drawImage(img, j * img.width, img.height/2);
-              console.log(context.getImageData(j * img.width, img.height/2, img.width, img.height), "Image data?");
-            }
-        })(img,j);
+        var element = document.createElement("img");
+        element.setAttribute("src", gamesArray[j].videoThumbnails[0].content);
+        element.setAttribute("height", gamesArray[j].videoThumbnails[0].height);
+        element.setAttribute("width", gamesArray[j].videoThumbnails[0].width);
+        element.setAttribute("alt", gamesArray[j].homeTeamName + ' vs ' + gamesArray[j].awayTeamName);
+        mlb.container.appendChild(element);
       }
-
 
       document.addEventListener('keydown', doKeyEvent, true);
       document.addEventListener('keyup' , doKeyUp, true);
 
       function doKeyEvent(event){
         console.log('clicky happened');
+        var x = event.offsetX;
+        var y = event.offsetY;
         event = event || window.event;
         if(event.keyCode ===37){
           console.log('left arrow');
-          console.log(mlb.canvas.getBoundingClientRect(), 'canvas left key')
+          // console.log(mlb.canvas.getBoundingClientRect(), 'canvas left key')
+
+          console.log(x, y, 'offsets')
           //go backwards
         } else if (event.keyCode === 39){
           console.log('right arrow ');
-            console.log(mlb.canvas.getBoundingClientRect(), 'canvas right key')
+            // console.log(mlb.canvas.getBoundingClientRect(), 'canvas right key')
           //go forwards
+          console.log(x, y, 'offsets')
         }
       }
-      function doKeyup(){
+      function doKeyUp(){
         console.log( 'keyup');
         // stop scrolling
       }
-    }
+
   }
 })();
 
-
+//TODO refactor without canvas to make it work first
 //TODO make images selectable
 //TODO scroll through images
   //center img starts big, when scrolling, whatever img is there, becomes big --> need selected variable?
